f12f448f387b6e33eb71ff1339e3c6000b017a84
dominicporter <337-dominicporter@users.noreply.code.organise.earth>
2023-12-03 10:17:34 +0000

    Day 3 Part 2

--- day3.js
+++ day3.js
@@ -1,4 +1,4 @@
-export const calculateDay3 = (input) => {
+export const calculateDay3Part1 = (input) => {
   // split it into arrays
   const rows = input
     .split('')
@@ -16,18 +16,64 @@ export const calculateDay3 = (input) => {
   const validPartNumbers = partNumbers.filter((p) => hasSymbolNearby(p, rows));
 
   // add them together and return the result
-  console.log(validPartNumbers.length);
   return validPartNumbers.reduce((acc, num) => acc + parseInt(num.num), 0);
+};
+export const calculateDay3Part2 = (input) => {
+  // split it into arrays
+  const rows = input
+    .split('')
+    .filter((c) => c !== ' ')
+    .join('')
+    .split('\n');
+
+  // get the numbers and their coords
+  const partNumbers = rows.reduce((acc, row, rowIndex) => {
+    const numsInRow = findNumbers(row, rowIndex);
+    return [...acc, ...numsInRow];
+  }, []);
+
+  // find all the stars
+  const stars = rows.reduce((acc, row, rowIndex) => {
+    const starsInRow = findStars(row, rowIndex);
+    return [...acc, ...starsInRow];
+  }, []);
+  console.log('all',stars);
+
+  // for each number calulate if they have a symbol nearby
+  const starMap = stars.map((star) => {
+    const adjacentParts = partNumbers.filter((p) => isAdjacent(p, star));
+    console.log(adjacentParts)
+    return adjacentParts.length === 2? adjacentParts[0].num * adjacentParts[1].num : 0;
+  });
+
+  console.log(starMap);
+
+  // add them together and return the result
+  return starMap.reduce((acc, num) => acc + parseInt(num), 0);
+};
+
+const isAdjacent = (pn, star) => {
+    // console.log({pn, star})
+  return (
+    (pn.y === star.y - 1 &&
+      star.x >= pn.x - 1 &&
+      star.x <= pn.x + pn.num.length) ||
+    (pn.y === star.y + 1 &&
+      star.x >= pn.x - 1 &&
+      star.x <= pn.x + pn.num.length) ||
+    (pn.y === star.y &&
+      (pn.x === star.x + 1 || pn.x + pn.num.length === star.x))
+  );
 };
 
 const hasSymbolNearby = (partNumber, rows) => {
-//   console.log({
-//     partNumber: partNumber.num,
-//     hasLeft: hasLeft(partNumber, rows),
-//     hasRight: hasRight(partNumber, rows),
-//     hasAbove: hasAbove(partNumber, rows),
-//     hasBelow: hasBelow(partNumber, rows),
-//   });
+  //   console.log({
+  //     partNumber: partNumber.num,
+  //     hasLeft: hasLeft(partNumber, rows),
+  //     hasRight: hasRight(partNumber, rows),
+  //     hasAbove: hasAbove(partNumber, rows),
+  //     hasBelow: hasBelow(partNumber, rows),
+  //   });
   return (
     hasLeft(partNumber, rows) ||
     hasRight(partNumber, rows) ||
@@ -54,6 +100,12 @@ const findNumbers = (row, rowIndex) => {
     { partial: null, nums: [] }
   );
   return [...numsInRow.nums, numsInRow.partial].filter((n) => n);
+};
+const findStars = (row, rowIndex) => {
+  const starsInRow = row.split('').reduce((acc, char, columnIndex) => {
+    return char === '*' ? [...acc, { x: columnIndex, y: rowIndex }] : acc;
+  }, []);
+  return starsInRow;
 };
 
 function hasBelow(partNumber, rows) {
--- day3.test.js
+++ day3.test.js
@@ -1,10 +1,10 @@
-import { day3Data } from "./data";
-import { calculateDay3 } from "./day3";
+import { day3Data } from './data';
+import { calculateDay3Part1, calculateDay3Part2 } from './day3';
 
 describe('Day3', () => {
-  it('returns correct value for sample input', () => {
-    const input = 
-    `467..114..
+  describe('Part 1', () => {
+    it('returns correct value for sample input', () => {
+      const input = `467..114..
     ...*......
     ..35..633.
     ......#...
@@ -15,18 +15,45 @@ describe('Day3', () => {
     ...$.*....
     .664.598..`;
 
-    expect(calculateDay3(input)).toEqual(4361);
-  });
-  it('returns correct value input with nums in right col', () => {
-    const input = 
-    `
+      expect(calculateDay3Part1(input)).toEqual(4361);
+    });
+    it('returns correct value input with nums in right col', () => {
+      const input = `
     ..1
     .*.`;
 
-    expect(calculateDay3(input)).toEqual(1);
+      expect(calculateDay3Part1(input)).toEqual(1);
+    });
+    it('returns correct value for real input', () => {
+      expect(calculateDay3Part1(day3Data)).toEqual(554003);
+    });
   });
-  it('returns correct value for real input', () => {
-  
-    expect(calculateDay3(day3Data)).toEqual(554003);
+
+  describe('Part 2', () => {
+    it('returns correct value for sample input', () => {
+      const input = `467..114..
+    ...*......
+    ..35..633.
+    ......#...
+    617*......
+    .....+.58.
+    ..592.....
+    ......755.
+    ...$.*....
+    .664.598..`;
+
+      expect(calculateDay3Part2(input)).toEqual(467835);
+    });
+    it('edge case', () => {
+      const input = `
+      ...........
+      ...428*522.
+      ...........`;
+
+      expect(calculateDay3Part2(input)).toEqual(223416);
+    });
+    it('returns correct value for real input', () => {
+      expect(calculateDay3Part2(day3Data)).toEqual(87263515);
+    });
   });
 });
