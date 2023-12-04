7b76692959620308a0fd8a5aaf289c5e9e6812d6
dominicporter <337-dominicporter@users.noreply.code.organise.earth>
2023-12-04 23:54:07 +0000

    Day 4 Part 2

--- day4.js
+++ day4.js
@@ -1,21 +1,15 @@
 export const calculateDay4Part1 = (input) => {
   const allCardScores = input
     .split('\n')
-    .map((line) => calculateScoreForCard(line));
+    .map((line) => calculatePart1ScoreForCard(line));
 
   return allCardScores.reduce((acc, num) => acc + parseInt(num), 0);
 };
 
-const calculateScoreForCard = (line) => {
+const calculatePart1ScoreForCard = (line) => {
   const card = line.split(': ')[1].split(' | ');
-  const winningNos = card[0]
-    .split(' ')
-    .filter((n) => n)
-    .map((n) => parseInt(n));
-  const haveNos = card[1]
-    .split(' ')
-    .filter((n) => n)
-    .map((n) => parseInt(n));
+  const winningNos = getWinningNumbers(card);
+  const haveNos = getHaveNumbers(card);
 
   const score = haveNos.reduce((score, num) => {
     if (winningNos.includes(num)) {
@@ -24,53 +18,73 @@ const calculateScoreForCard = (line) => {
     return score;
   }, 0);
 
-  console.log({ winningNos, haveNos, score });
+  //   console.log({ winningNos, haveNos, score });
+  return score;
+};
+const calculatePart2MatchesForCard = (line) => {
+  const card = line.split(': ')[1].split(' | ');
+  const winningNos = getWinningNumbers(card);
+  const haveNos = getHaveNumbers(card);
+
+  const score = haveNos.filter((num) => winningNos.includes(num)).length;
+
+  //   console.log({ winningNos, haveNos, score });
   return score;
 };
 
+const cache = new Map();
+
 export const calculateDay4Part2 = (input) => {
+  cache.clear();
   const cardLines = input.split('\n');
-  const allCardCounts = cardLines.map((_, i) =>
-    calculateTotalCardsWon(cardLines, i)
+  const cardMatches = cardLines.map((line) =>
+    calculatePart2MatchesForCard(line)
   );
-  console.log({ allCardCounts });
+  console.log({ cardMatches });
 
-  return allCardCounts.reduce((acc, num) => acc + parseInt(num), 0);
+  const cardScores = cardMatches.map((_, i) =>
+    addCardsWonForThisCard(cardMatches, i)
+  );
+  // console.log({cardWinCounts});
+  console.log({ cardScores });
+
+  return Array.from(cardScores.values()).reduce(
+    (acc, num) => acc + parseInt(num),
+    0
+  );
 };
 
-const cardWinCounts = new Map();
+const addCardsWonForThisCard = (cardMatches, cardId) => {
+  if (cache.has(cardId)) return cache.get(cardId);
 
-const calculateTotalCardsWon = (cardLines, i) => {
-  const line = cardLines[i];
-  const cardId = parseInt(line.split(':')[0].split(' ').slice(-1));
-  const cachedCount = cardWinCounts.get(cardId);
-  //   console.log({ cachedCount });
-  if (cachedCount) return cachedCount;
-
-  const card = line.split(': ')[1].split(' | ');
-  const winningNos = card[0]
-    .split(' ')
-    .filter((n) => n)
-    .map((n) => parseInt(n));
-  const haveNos = card[1]
-    .split(' ')
-    .filter((n) => n)
-    .map((n) => parseInt(n));
-
-  // calculate how many winning cards there are
-  const winsForThisCard = haveNos.filter((num) =>
-    winningNos.includes(num)
-  ).length;
-  // recurse into the others
-  const winsForOtherCards = new Array(winsForThisCard)
+  const newCardsToAdd = new Array(cardMatches[cardId])
     .fill(cardId)
-    .map((n, i) => n + i)
-    .map((id) => calculateTotalCardsWon(cardLines, id));
+    .map((n, i) => n + i + 1);
 
-    const totalWinsForThisCard =
-    1 +
-    winsForOtherCards.reduce((acc, num) => acc + num, 0);
-    cardWinCounts.set(cardId, totalWinsForThisCard);
-    console.log({ cardId, winsForThisCard, winsForOtherCards, totalWinsForThisCard });
-  return totalWinsForThisCard;
+  //   console.log({cardId,newCardsToAdd})
+  const newCardsFromOtherNewCards = newCardsToAdd.reduce((acc, nextId) => {
+    return acc + addCardsWonForThisCard(cardMatches, nextId);
+  }, 0);
+  //   console.log({
+  //     cardId: cardId + 1,
+  //     matches: cardMatches[cardId],
+  //     newCardsFromOtherNewCards,
+  //   });
+  const scoreForCard = 1 + newCardsFromOtherNewCards;
+  cache.set(cardId, scoreForCard);
+  return scoreForCard;
 };
+
+function getHaveNumbers(card) {
+  return card[1]
+    .split(' ')
+    .filter((n) => n)
+    .map((n) => parseInt(n));
+}
+
+function getWinningNumbers(card) {
+  return card[0]
+    .split(' ')
+    .filter((n) => n)
+    .map((n) => parseInt(n));
+}
