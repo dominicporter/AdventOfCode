6eb679a2741462dbb44332ff65bc1be3aa99be12
dominicporter <337-dominicporter@users.noreply.code.organise.earth>
2023-12-04 15:25:57 +0000

    not working Day 4 part 2

--- day4.js
+++ day4.js
@@ -27,3 +27,50 @@ const calculateScoreForCard = (line) => {
   console.log({ winningNos, haveNos, score });
   return score;
 };
+
+export const calculateDay4Part2 = (input) => {
+  const cardLines = input.split('\n');
+  const allCardCounts = cardLines.map((_, i) =>
+    calculateTotalCardsWon(cardLines, i)
+  );
+  console.log({ allCardCounts });
+
+  return allCardCounts.reduce((acc, num) => acc + parseInt(num), 0);
+};
+
+const cardWinCounts = new Map();
+
+const calculateTotalCardsWon = (cardLines, i) => {
+  const line = cardLines[i];
+  const cardId = parseInt(line.split(':')[0].split(' ').slice(-1));
+  const cachedCount = cardWinCounts.get(cardId);
+  //   console.log({ cachedCount });
+  if (cachedCount) return cachedCount;
+
+  const card = line.split(': ')[1].split(' | ');
+  const winningNos = card[0]
+    .split(' ')
+    .filter((n) => n)
+    .map((n) => parseInt(n));
+  const haveNos = card[1]
+    .split(' ')
+    .filter((n) => n)
+    .map((n) => parseInt(n));
+
+  // calculate how many winning cards there are
+  const winsForThisCard = haveNos.filter((num) =>
+    winningNos.includes(num)
+  ).length;
+  // recurse into the others
+  const winsForOtherCards = new Array(winsForThisCard)
+    .fill(cardId)
+    .map((n, i) => n + i)
+    .map((id) => calculateTotalCardsWon(cardLines, id));
+
+    const totalWinsForThisCard =
+    1 +
+    winsForOtherCards.reduce((acc, num) => acc + num, 0);
+    cardWinCounts.set(cardId, totalWinsForThisCard);
+    console.log({ cardId, winsForThisCard, winsForOtherCards, totalWinsForThisCard });
+  return totalWinsForThisCard;
+};
