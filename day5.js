0a0750b07d5b745210d0f236aea31254543a5308
dominicporter <337-dominicporter@users.noreply.code.organise.earth>
2023-12-05 20:46:15 +0000

    Day 5 Part 2

--- day5.js
+++ day5.js
@@ -2,19 +2,91 @@ export const calculateDay5Part1 = (input) => {
   // Parse the input
   const lines = input.split('\n');
   const seeds = getNumbers(lines[0].split(': ')[1]);
+  const maps = buildMapsStructure(lines);
+
+  maps.map((map) => map.sort((a, b) => a.src - b.src));
+
+  // pass through all the maps
+  const mappedSeeds = seeds.map((seed) => processSeed(seed, maps));
+
+  //work out lowest
+  return Math.min(...mappedSeeds);
+};
+export const calculateDay5Part2 = (input) => {
+  // Parse the input
+  const lines = input.split('\n');
+  const seeds = getNumbers(lines[0].split(': ')[1]);
   console.log(seeds);
 
+  const seedPairs = seeds.reduce((acc, seed, i) => {
+    if (i % 2 === 0) {
+      return [...acc, { start: seed, length: seeds[i + 1] }];
+    }
+    return acc;
+  }, []);
+
+//   console.log(seedPairs);
+  const maps = buildMapsStructure(lines);
+
+  // pass through all the maps
+  return seedPairs.reduce((acc, { start, length }) => {
+    return Math.min(acc, processSeedsInRange(start, length, maps));
+  }, Infinity);
+
+};
+
+const processSeedsInRange = (start, length, maps) => {
+    console.log({start, length});
+  let lowest = Infinity;
+  for (let i = start; i < start + length; i++) {
+    const mappedSeed = processSeed(i, maps);
+    if (mappedSeed < lowest) {
+      lowest = mappedSeed;
+    }
+  }
+  return lowest;
+};
+const processSeed = (seed, maps) => {
+    // console.log({seed});
+  return maps.reduce((acc, map) => {
+    const mappedSeed = mapSeed(acc, map);
+    //   console.log({mappedSeed});
+    return mappedSeed;
+  }, seed);
+};
+
+const mapSeed = (seed, map) => {
+  // console.log({seed, map});
+  for (const range of map) {
+    // console.log({range})
+    if (seed < range.src) {
+      return seed;
+    }
+    if (seed >= range.src && seed < range.src + range.len) {
+      return range.dest + (seed - range.src);
+    }
+  }
+
+  // no ranges matched so return seed
+  return seed;
+};
+const getNumbers = (line) =>
+  line
+    .split(' ')
+    .map((n) => parseInt(n))
+    .filter((n) => !isNaN(n));
+
+function buildMapsStructure(lines) {
   const maps = lines.slice(2).reduce((acc, line) => {
     if (line.includes('map')) {
       return [...acc, []];
     }
     const nums = getNumbers(line);
-    // console.log(nums)
+
     if (nums.length) {
       const currentMap = acc[acc.length - 1];
       const others = acc.slice(0, acc.length - 1);
 
-      console.log(currentMap);
       return [
         ...others,
         [...currentMap, { dest: nums[0], src: nums[1], len: nums[2] }],
@@ -23,43 +95,7 @@ export const calculateDay5Part1 = (input) => {
     return acc;
   }, []);
 
-//   console.log(JSON.stringify(maps, null, 2));
   // sort by source
   maps.map((map) => map.sort((a, b) => a.src - b.src));
-//   console.log(JSON.stringify(maps, null, 2));
-
-  // pass through all the maps
-  const mappedSeeds = seeds.map((seed) => processSeed(seed, maps));
-  console.log({mappedSeeds});
-  //work out lowest
-  return Math.min(...mappedSeeds);
-};
-
-const processSeed = (seed, maps) => {
-  return maps.reduce((acc, map) => {
-      const mappedSeed = mapSeed(acc, map);
-    //   console.log({mappedSeed});
-    return mappedSeed;
-  }, seed);
-};
-
-const mapSeed = (seed, map) => {
-    // console.log({seed, map});
-    for (const range of map) {
-        // console.log({range})
-        if (seed < range.src) {
-            return seed
-        }
-        if (seed >=range.src && seed < range.src + range.len) {
-            return range.dest + (seed - range.src)
-        }
-    }
-
-    // no ranges matched so return seed
-    return seed
+  return maps;
 }
-const getNumbers = (line) =>
-  line
-    .split(' ')
-    .map((n) => parseInt(n))
-    .filter((n) => !isNaN(n));
--- day5.test.js
+++ day5.test.js
@@ -1,5 +1,5 @@
 import { day5Data } from "./data";
-import { calculateDay5Part1 } from "./day5";
+import { calculateDay5Part1, calculateDay5Part2 } from "./day5";
 
 describe('Day5', () => {
   describe('Part 1', () => {
@@ -42,7 +42,51 @@ describe('Day5', () => {
     });
 
     it('returns correct value for real input', () => {
-        expect(calculateDay5Part1(day5Data)).toEqual(13768818);
+        expect(calculateDay5Part1(day5Data)).toEqual(3374647);
+      });
+  });
+  describe('Part 2', () => {
+    it('returns correct value for sample input', () => {
+      const input = `seeds: 79 14 55 13
+
+        seed-to-soil map:
+        50 98 2
+        52 50 48
+        
+        soil-to-fertilizer map:
+        0 15 37
+        37 52 2
+        39 0 15
+        
+        fertilizer-to-water map:
+        49 53 8
+        0 11 42
+        42 0 7
+        57 7 4
+        
+        water-to-light map:
+        88 18 7
+        18 25 70
+        
+        light-to-temperature map:
+        45 77 23
+        81 45 19
+        68 64 13
+        
+        temperature-to-humidity map:
+        0 69 1
+        1 0 69
+        
+        humidity-to-location map:
+        60 56 37
+        56 93 4`;
+
+      expect(calculateDay5Part2(input)).toEqual(46);
+    });
+
+    // This takes a long time to run
+    it.skip('returns correct value for real input', () => {
+        expect(calculateDay5Part2(day5Data)).toEqual(6082852);
       });
   });
 });
