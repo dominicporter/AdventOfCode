96718a67bfe31374656229e29f100f717dfbee61
dominicporter <337-dominicporter@users.noreply.code.organise.earth>
2023-12-10 08:41:45 +0000

    Day 10 Part 1

--- day10.js
+++ day10.js
@@ -1,6 +1,63 @@
 export const calculateDay10Part1 = (input) => {
+  let startPos = { x: 0, y: 0 };
+  const grid = input.split('\n').map((l, y) => {
+    const line = l.trim();
+    const row = line.split('');
+    const sPos = line.indexOf('S');
+    if (sPos > -1) {
+      startPos = { x: sPos, y };
+    }
+    return row;
+  });
 
+  let count = 0;
+
+  console.log({ grid, startPos });
+
+  let prevPos = { x: 0, y: 0 };
+  let pos = { ...startPos };
+  while (true) {
+    const nextPos = getNextTile(grid, prevPos, pos);
+    console.log({ nextPos });
+    console.log({ nextTile: grid[nextPos.y][nextPos.x] });
+    count++;
+    if (grid[nextPos.y][nextPos.x] === 'S') {
+      return count / 2;
+    }
+    prevPos = pos;
+    pos = nextPos;
+  }
 };
-export const calculateDay10Part2 = (input) => {
-    
+
+const getNextTile = (grid, prevPos, { x, y }) => {
+  const tile = grid[y][x];
+  if (tile === 'S') {
+    if (x < grid[y].length && ['-', '7', 'J'].includes(grid[y][x + 1]))
+      return { x: x + 1, y: y };
+    if (x > 0 && ['-', 'L', 'F'].includes(grid[y][x - 1]))
+      return { x: x - 1, y: y };
+    if (y > grid.length && ['|', 'J', 'L'].includes(grid[y - 1][x]))
+      return { x: x, y: y + 1 };
+    if (y > 0 && ['|', '7', 'F'].includes(grid[y - 1][x]))
+      return { x: x, y: y - 1 };
+  }
+
+  const LEFT = { x: x - 1, y };
+  const DOWN = { x, y: y + 1 };
+  const UP = { x, y: y - 1 };
+  const RIGHT = { x: x + 1, y };
+  if (tile === '7') {
+    return prevPos.x !== x - 1 || prevPos.y !== y ? LEFT : DOWN;
+  } else if (tile === 'J') {
+    return prevPos.x !== x - 1 || prevPos.y !== y ? LEFT : UP;
+  } else if (tile === 'L') {
+    return prevPos.x !== x + 1 || prevPos.y !== y ? RIGHT : UP;
+  } else if (tile === 'F') {
+    return prevPos.x !== x + 1 || prevPos.y !== y ? RIGHT : DOWN;
+  } else if (tile === '|') {
+    return prevPos.x !== x || prevPos.y !== y - 1 ? UP : DOWN;
+  } else if (tile === '-') {
+    return prevPos.x !== x - 1 || prevPos.y !== y ? LEFT : RIGHT;
+  }
 };
+export const calculateDay10Part2 = (input) => {};
--- day10.test.js
+++ day10.test.js
@@ -2,14 +2,26 @@ import { Day10Data } from './data';
 import { calculateDay10Part1, calculateDay10Part2 } from './day10';
 
 describe('Day10', () => {
-  const exampleInput1 = ``;
+  const exampleInput1 = `-L|F7
+  7S-7|
+  L|7||
+  -L-J|
+  L|-JF`;
+  const exampleInput2 = `7-F7-
+  .FJ|7
+  SJLL7
+  |F--J
+  LJ.LJ`;
   describe('Part 1', () => {
     it('returns correct value for sample input 1', () => {
-      expect(calculateDay10Part1(exampleInput1)).toEqual(0);
+      expect(calculateDay10Part1(exampleInput1)).toEqual(4);
+    });
+    it('returns correct value for sample input 2', () => {
+      expect(calculateDay10Part1(exampleInput2)).toEqual(8);
     });
 
     it('returns correct value for real input', () => {
-      expect(calculateDay10Part1(Day10Data)).toEqual(0);
+      expect(calculateDay10Part1(Day10Data)).toEqual(6649);
     });
   });
   describe('Part 2', () => {
