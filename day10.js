cd5bc9bda7130a4bb75c700d04250acef602c9e9
dominicporter <337-dominicporter@users.noreply.code.organise.earth>
2023-12-12 07:30:10 +0000

    Day 10

--- day10.js
+++ day10.js
@@ -18,8 +18,6 @@ export const calculateDay10Part1 = (input) => {
   let pos = { ...startPos };
   while (true) {
     const nextPos = getNextTile(grid, prevPos, pos);
-    console.log({ nextPos });
-    console.log({ nextTile: grid[nextPos.y][nextPos.x] });
     count++;
     if (grid[nextPos.y][nextPos.x] === 'S') {
       return count / 2;
@@ -60,4 +58,53 @@ const getNextTile = (grid, prevPos, { x, y }) => {
     return prevPos.x !== x - 1 || prevPos.y !== y ? LEFT : RIGHT;
   }
 };
-export const calculateDay10Part2 = (input) => {};
+export const calculateDay10Part2 = (input) => {
+  let startPos = { x: 0, y: 0 };
+  const grid = input.split('\n').map((l, y) => {
+    const line = l.trim();
+    const row = line.split('');
+    const sPos = line.indexOf('S');
+    if (sPos > -1) {
+      startPos = { x: sPos, y };
+    }
+    return row;
+  });
+
+  console.log({ grid, startPos });
+
+  // make a copy of the grid
+  const gridCopy = grid.map((row) => row.map(() => '.'));
+  console.log({ gridCopy });
+
+  let prevPos = { x: 0, y: 0 };
+  let pos = { ...startPos };
+  while (true) {
+    const nextPos = getNextTile(grid, prevPos, pos);
+    gridCopy[nextPos.y][nextPos.x] = grid[nextPos.y][nextPos.x];
+    if (grid[nextPos.y][nextPos.x] === 'S') {
+      break;
+    }
+    prevPos = pos;
+    pos = nextPos;
+  }
+  let count = 0;
+  gridCopy.map((row, y) => {
+    let pipes = 0;
+    let lastPipe = '';
+    row.map((tile) => {
+      if (tile === '.' && pipes % 2 === 1) {
+        count++;
+      } else if ('SF7LJ|'.includes(tile)) {
+        if (
+          !(lastPipe == 'L' && tile === '7') &&
+          !(lastPipe == 'F' && tile === 'J')
+        ) {
+          pipes++;
+        }
+        lastPipe = tile;
+      }
+    });
+  });
+
+  return count;
+};
--- day10.test.js
+++ day10.test.js
@@ -2,17 +2,17 @@ import { Day10Data } from './data';
 import { calculateDay10Part1, calculateDay10Part2 } from './day10';
 
 describe('Day10', () => {
-  const exampleInput1 = `-L|F7
-  7S-7|
-  L|7||
-  -L-J|
-  L|-JF`;
-  const exampleInput2 = `7-F7-
-  .FJ|7
-  SJLL7
-  |F--J
-  LJ.LJ`;
   describe('Part 1', () => {
+    const exampleInput1 = `-L|F7
+    7S-7|
+    L|7||
+    -L-J|
+    L|-JF`;
+    const exampleInput2 = `7-F7-
+    .FJ|7
+    SJLL7
+    |F--J
+    LJ.LJ`;
     it('returns correct value for sample input 1', () => {
       expect(calculateDay10Part1(exampleInput1)).toEqual(4);
     });
@@ -25,12 +25,70 @@ describe('Day10', () => {
     });
   });
   describe('Part 2', () => {
-    it('returns correct value for sample input', () => {
-      expect(calculateDay10Part2(exampleInput1)).toEqual(0);
+    it('returns correct value for sample input1', () => {
+      expect(
+        calculateDay10Part2(
+         `...........
+          .S-------7.
+          .|F-----7|.
+          .||.....||.
+          .||.....||.
+          .|L-7.F-J|.
+          .|..|.|..|.
+          .L--J.L--J.
+          ...........`)
+      ).toEqual(4);
+    });
+
+    it('returns correct value for sample input 2', () => {
+      expect(
+        calculateDay10Part2(`..........
+      .S------7.
+      .|F----7|.
+      .||....||.
+      .||....||.
+      .|L-7F-J|.
+      .|..||..|.
+      .L--JL--J.
+      ..........`)
+      ).toEqual(4);
+    });
+
+    it('returns correct value for sample input 3', () => {
+      expect(
+        calculateDay10Part2(
+          `.F----7F7F7F7F-7....
+          .|F--7||||||||FJ....
+          .||.FJ||||||||L7....
+          FJL7L7LJLJ||LJ.L-7..
+          L--J.L7...LJS7F-7L7.
+          ....F-J..F7FJ|L7L7L7
+          ....L7.F7||L7|.L7L7|
+          .....|FJLJ|FJ|F7|.LJ
+          ....FJL-7.||.||||...
+          ....L---J.LJ.LJLJ...`
+        )
+      ).toEqual(8);
+    });
+    it('returns correct value for sample input 4', () => {
+      expect(
+        calculateDay10Part2(
+          `FF7FSF7F7F7F7F7F---7
+          L|LJ||||||||||||F--J
+          FL-7LJLJ||||||LJL-77
+          F--JF--7||LJLJ7F7FJ-
+          L---JF-JLJ.||-FJLJJ7
+          |F|F-JF---7F7-L7L|7|
+          |FFJF7L7F-JF7|JL---7
+          7-L-JL7||F7|L7F-7F7|
+          L.L7LFJ|||||FJL7||LJ
+          L7JLJL-JLJLJL--JLJ.L`
+        )
+      ).toEqual(10);
     });
 
     it('returns correct value for real input', () => {
-      expect(calculateDay10Part2(Day10Data)).toEqual(0);
+      expect(calculateDay10Part2(Day10Data)).toEqual(601);
     });
   });
 });
